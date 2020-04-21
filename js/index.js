c521151e70366151b0bc5719d1db68af0825bc1a
yagobmarques <yagobsmarques@gmail.com>
2020-04-21 16:23:43 -0300

    primeira parte do site

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, "");
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, "");
+  });
